8cb80066f5810fb15d33dfcd90a269332a836fd9
Yasir Khan <yasirkhanone22@gmail.com>
2022-08-11 23:06:04 +0500

    eventlistener added

--- multiplier__simple/main.js
+++ multiplier__simple/main.js
@@ -2,20 +2,23 @@ const num1 = Math.ceil(Math.random() * 10);
 const num2 = Math.ceil(Math.random() * 10);
 
 const heading1 = document.getElementById("question");
-heading1.innerText = `Multiply ${num1} by ${num2}`;
+const inputElem = document.getElementById("input");
+const formElem = document.getElementById("form");
 
-const input__Element = document.getElementById("input");
-const form__element = document.getElementById("form");
 const Answers = num1 * num2;
 console.log(Answers)
 
 
-// form__element.addEventListener("submit", () => {
-//     if (Answers == +input__Element.value) {
-//         score++;
-//         console.log("submit clicked!")
-//         console.log(input__Element)
-//     } else {
-//         score--
-//     }
-// })
+heading1.innerText = `Multiply ${num1} by ${num2}?`;
+
+
+formElem.addEventListener("submit", () => {
+    const UserAns = +inputElem.value;
+    if (Answers == UserAns) {
+        score++;
+        console.log("submit clicked!")
+        console.log(inputElem)
+    } else {
+        score--
+    }
+})
