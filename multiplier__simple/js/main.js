4c2df3ad64be11e1284bdbdf3854f38f67ed8f58
Yasir Khan <yasirkhanone22@gmail.com>
2022-08-12 00:08:48 +0500

    js files modify

--- multiplier__simple/js/main.js
+++ multiplier__simple/js/main.js
@@ -2,10 +2,16 @@ const num1 = Math.ceil(Math.random() * 10);
 const num2 = Math.ceil(Math.random() * 10);
 
 const formElem = document.getElementById("form");
-const inputElem = document.getElementById("input");
 const heading1 = document.getElementById("question");
+const inputElem = document.getElementById("input");
+
+const scoreElem = document.getElementById("score")
 
 let score = JSON.parse(localStorage.getItem("score"));
+if (!score) {
+    score = 0;
+}
+scoreElem.innerText = `score: ${score}`;
 
 heading1.innerText = `Multiply ${num1} by ${num2}?`;
 
