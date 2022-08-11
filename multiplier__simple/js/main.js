9231888191d9b28ee7054aa1a23e196cae936714
Yasir Khan <yasirkhanone22@gmail.com>
2022-08-12 00:00:23 +0500

    js modified

--- multiplier__simple/js/main.js
+++ multiplier__simple/js/main.js
@@ -5,13 +5,29 @@ const formElem = document.getElementById("form");
 const inputElem = document.getElementById("input");
 const heading1 = document.getElementById("question");
 
-
+let score = JSON.parse(localStorage.getItem("score"));
 
 heading1.innerText = `Multiply ${num1} by ${num2}?`;
 
 const Answers = num1 * num2;
 
 formElem.addEventListener("submit", () => {
-    const UserAns = inputElem.value;
-    console.log(UserAns, typeof UserAns)
-})
+    const UserAns = +inputElem.value;
+
+    if (Answers === UserAns) {
+        score++;
+        console.log(score)
+        updateLocalStorage()
+    }
+    else {
+        score--
+        console.log(score)
+        updateLocalStorage()
+
+    }
+});
+
+
+function updateLocalStorage() {
+    localStorage.setItem("score", JSON.stringify(score))
+}
