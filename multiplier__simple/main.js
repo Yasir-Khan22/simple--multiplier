cf5beb36adb0bc907905d0bc6415cce758247a52
Yasir Khan <yasirkhanone22@gmail.com>
2022-08-11 22:02:09 +0500

    js updated

--- multiplier__simple/main.js
+++ multiplier__simple/main.js
@@ -1,1 +1,20 @@
-console.log("Mutipliyer in Javascript!")
+const num1 = Math.ceil(Math.random() * 10);
+const num2 = Math.ceil(Math.random() * 10);
+
+const heading1 = document.getElementById("question");
+heading1.innerText = `Multiply ${num1} by ${num2}`;
+
+const input__Element = document.getElementById("input");
+const form__element = document.getElementById("form");
+const Answers = num1 * num2;
+
+
+form__element.addEventListener("submit", () => {
+    if (Answers == +input__Element.value) {
+        score++;
+        console.log("submit clicked!")
+        console.log(input__Element)
+    } else {
+        score--
+    }
+})
